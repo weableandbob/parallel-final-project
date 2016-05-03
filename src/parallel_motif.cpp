8b961c9f7d412a988ae4497128e8510fd1676c3b
Brian Sheedy <weableandbob@gmail.com>
2016-05-03 00:24:23 -0400

    Started fleshing out code, confirmed to compile and run on kratos

--- src/parallel_motif.cpp
+++ src/parallel_motif.cpp
@@ -26,6 +26,8 @@ extern "C" {
 #define MPIT_RANK_DONE 0
 #define MPIT_THREAD_DONE 1
 #define MPIT_THREAD_MOVE 2
+#define MPIT_REQUEST_NEIGHBORS 3
+#define MPIT_RESPONSE_NEIGHBORS 4
 
 //Macros
 #define CLEANUP_TP \
@@ -35,6 +37,12 @@ extern "C" {
     CLEANUP_TP \
     MPI_Finalize(); \
     exit(EXIT_FAILURE);
+
+#define CHECK_MUTEX_INIT(value) \
+    if(value != 0){ \
+        cerr << "Failed to initialize mutex with error code " << value << endl; \
+        CLEAN_EXIT \
+    }
 
 using namespace std;
 
@@ -54,10 +62,10 @@ struct motif_node {
 
 struct dfs_data {
     int motif_index; //Index in the global counter array
-    struct graph_node; //The graph node being visited on this dfs call
+    struct graph_node cur_node; //The graph node being visited on this dfs call
     struct motif_node desired_motif_node; //The motif node id and role that this node should match
-    //bool first_invocation; //Whether this is the first time DFS is being run
-    list< pair<struct motif_node, struct motif_node> > motif_edges; //List of motif edges left to be evaluated
+    bool first_invocation; //Whether this is the first time DFS is being run
+    vector< pair<struct motif_node, struct motif_node> > motif_edges; //List of motif edges left to be evaluated
     set<int> visited_nodes; //Set of all node numbers that have been visited in this path
     map<int, int> motif_to_unique; //Map of motif nodes to unique node IDs
 };
@@ -71,7 +79,6 @@ int mpi_commsize;
 
 //Threadpools
 threadpool g_local_threads; //Searches that started in this rank
-//threadpool g_remote_threads; //Searches that started due to an MPI message
 
 //Graph related variables
 int g_ranks_done; //Counter used to keep track of how many nodes are done with the current motif since barriers are not an option
@@ -81,9 +88,32 @@ vector<int> g_vtxdist; //Equivalent to vtxdist from ParMETIS. If vtxdist[i] <= n
                        //Ordered, so use a binary search
 
 //Synchronization
-//pthread_mutex_t* m_counter;
-//pthread_mutex_t* m_mpi_lock;
+pthread_mutex_t* m_counter_lock;
+pthread_mutex_t* m_mpi_lock;
 //map<pthread_t, sem_t*> g_locked_threads;
+
+/***************************************************************************/
+/* isValidMotif ************************************************************/
+/***************************************************************************/
+//Checks a possible motif for excess edges, aka edges between nodes in the motif that are not defined in the motif
+//If any such edges exist, the given set of vertices is not a valid motif
+/*Pseudocode:
+    for each visited node:
+        set motif_neighbors
+        if node is local:
+            copy neighbors that are also in visited set into motif_neighbors
+        else:
+            send request to applicable rank for node's neighbors that are also in the visited set
+            wait for response from main thread
+            copy response into motif_neighbors
+        for each neighbor in motif_neighbor:
+            if the motif does not have an edge from this node to neighbor:
+                return false
+        return true
+*/
+bool isValidMotif(dfs_data* data){
+    return true;
+}
 
 /***************************************************************************/
 /* threadDFS ***************************************************************/
@@ -99,6 +129,8 @@ vector<int> g_vtxdist; //Equivalent to vtxdist from ParMETIS. If vtxdist[i] <= n
         return
     (From here on, we know that we are at a valid node)
     if no edges left in motif: (this node was the last one needed)
+        if not valid motif:
+            return
         increment count for motif
         return
     check next edge in motif
@@ -132,7 +164,22 @@ vector<int> g_vtxdist; //Equivalent to vtxdist from ParMETIS. If vtxdist[i] <= n
             return*/
 void* threadDFS(void* d){
     struct dfs_data* data = (struct dfs_data*)d;
-    cout << "DFS run on motif " << data->motif_index << endl;
+
+    //Test code
+    cout << "Rank " << mpi_myrank << " DFS run on motif " << data->motif_index << " graph node role " << data->cur_node.unique_id;
+    cout << " desired motif node role " << data->desired_motif_node.motif_node_index << " motif edges ";
+    for(unsigned int i = 0; i < data->motif_edges.size(); i++){
+        cout << "([" << data->motif_edges[i].first.motif_node_index << ", ";
+        cout << data->motif_edges[i].first.role << "], [" << data->motif_edges[i].second.motif_node_index;
+        cout << ", " << data->motif_edges[i].second.role << "]) ";
+    }
+    cout << endl;
+    //End test code
+
+    //Current node and desired motif node role mismatch
+    if(data->cur_node.role != data->desired_motif_node.role){
+        return NULL;
+    }
 
     return NULL;
 }
@@ -143,18 +190,31 @@ void* threadDFS(void* d){
 //Thread function that walks the graph and searches for the given motif
 void* threadDispatcher(void* motif_index){
     //Visit each node in the local graph and search for motifs
-    for(int i = 0; i < 10; i++){
-
+    for(map<int, graph_node>::iterator it = g_local_nodes.begin(); it != g_local_nodes.end(); it++){
         struct dfs_data* data = new struct dfs_data;
+
         data->motif_index = *(int*)motif_index;
+        data->cur_node = it->second;
         data->first_invocation = true;
+        data->motif_edges = g_motifs[data->motif_index];
+        data->desired_motif_node = data->motif_edges[0].first;
+
         thpool_add_work(g_local_threads, threadDFS, (void*)data);
+        //Test code
+        sleep((mpi_myrank+1) * 3);
+        //End test code
     }
     //Wait for all locally dispatched threads to finish
     thpool_wait(g_local_threads);
-    //Wait for all ranks to finish then notify own rank
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Send(NULL, 0, MPI_INT, mpi_myrank, MPIT_RANK_DONE, MPI_COMM_WORLD);
+
+    //Notify all ranks of this rank's completion
+    //This would be so much better if we could just MPI_Barrier
+    MPI_Request req = MPI_REQUEST_NULL;
+    pthread_mutex_lock(m_mpi_lock);
+    for(int i = 0; i < mpi_commsize; i++){
+        MPI_Isend(NULL, 0, MPI_INT, i, MPIT_RANK_DONE, MPI_COMM_WORLD, &req);
+    }    
+    pthread_mutex_unlock(m_mpi_lock);
     return NULL;
 }
 
@@ -167,8 +227,8 @@ int main(int argc, char* argv[]){
 
     //Init MPI
     int mpi_thread_support;
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &mpi_thread_support);
-    if(mpi_thread_support != MPI_THREAD_MULTIPLE){
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &mpi_thread_support);
+    if(mpi_thread_support != MPI_THREAD_SERIALIZED){
         cout << "Did not receive the requested thread support" << endl;
         MPI_Finalize();
         exit(EXIT_FAILURE);
@@ -183,17 +243,44 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
     int num_local_threads = atoi(argv[3]);
-    //int num_remote_threads = atoi(argv[4]);
 
     //Initialize thread pools
     g_local_threads = thpool_init(num_local_threads);
-    //g_remote_threads = thpool_init(num_remote_threads);
+
+    //Initialize synchronization objects
+    m_counter_lock = new pthread_mutex_t;
+    rc = pthread_mutex_init(m_counter_lock, NULL);
+    CHECK_MUTEX_INIT(rc)
+    m_mpi_lock = new pthread_mutex_t;
+    rc = pthread_mutex_init(m_mpi_lock, NULL);
+    CHECK_MUTEX_INIT(rc)
+
+    //Test code: create dummy graph
+    for(int i = 0; i < 2; i++){
+        struct graph_node v;
+        v.unique_id = i;
+        v.role = 0;
+        for(int j = 0; j < 2; j++){
+            if(i == j){
+                continue;
+            }
+            v.neighbors.insert(j);
+        }
+        g_local_nodes[i] = v;
+    }
+    //End test code
 
     //Test code: create dummy motifs
-    for(unsigned int i = 0; i < 5; i++){
-        vector< pair<int, int> > motif;
-        for(unsigned int j = 0; j < 5; j++){
-            motif.push_back(make_pair(j, j));
+    for(unsigned int i = 0; i < 2; i++){
+        vector< pair<struct motif_node, struct motif_node> > motif;
+        for(unsigned int j = 0; j < 2; j++){
+            struct motif_node a;
+            a.motif_node_index = i;
+            a.role = 0;
+            struct motif_node b;
+            b.motif_node_index = j;
+            b.role = 0;
+            motif.push_back(make_pair(a, b));
         }
         g_motifs.push_back(motif);
     }
@@ -201,8 +288,11 @@ int main(int argc, char* argv[]){
 
     //Search for all motifs
     MPI_Status status;
+    int flag;
     int* motif_index = new int;
     for(unsigned int i = 0; i < g_motifs.size(); i++){
+        g_ranks_done = 0;
+
         //Create dispatcher thread for the motif
         pthread_t dispatcher;        
         *motif_index = i;
@@ -219,22 +309,40 @@ int main(int argc, char* argv[]){
 
         //Wait for any messages
         while(true){
-            rc = MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            //Look for any incoming messages
+            pthread_mutex_lock(m_mpi_lock);
+            rc = MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+            pthread_mutex_unlock(m_mpi_lock);
             if(rc != MPI_SUCCESS){
                 cout << "Failed to probe with error code " << rc << endl;
                 CLEAN_EXIT
             }
+            if(!flag){ //No messages in any queues
+                continue;
+            }
+
             //Check the tags -> Avoid switch/case since we need to break out of infinite loop
             if(status.MPI_TAG == MPIT_RANK_DONE){
-                //Clear message out of buffer
-                MPI_Recv(NULL, 0, MPI_INT, mpi_myrank, MPIT_RANK_DONE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                break;
+                //Clear message out of buffer and increment number of ranks done
+                pthread_mutex_lock(m_mpi_lock);
+                MPI_Recv(NULL, 0, MPI_INT, status.MPI_SOURCE, MPIT_RANK_DONE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                pthread_mutex_unlock(m_mpi_lock);
+                g_ranks_done++;
+                if(g_ranks_done == mpi_commsize){
+                    break; //All ranks are done, so break out of infinite loop
+                }
             }
             else if(status.MPI_TAG == MPIT_THREAD_MOVE){
-                //Create a new job in remote pool
+                //Spawn and detach a remote thread
             }
             else if(status.MPI_TAG == MPIT_THREAD_DONE){
                 //Unlock the specified thread
+            }
+            else if(status.MPI_TAG == MPIT_REQUEST_NEIGHBORS){
+                //Get requested neighbors and reply
+            }
+            else if(status.MPI_TAG == MPIT_RESPONSE_NEIGHBORS){
+                //Copy response into shared buffer and notify relevant thread
             }
             else{
                 cout << "Received unknown tag " << status.MPI_TAG << endl;
@@ -249,7 +357,6 @@ int main(int argc, char* argv[]){
 
     MPI_Barrier(MPI_COMM_WORLD);
     rc = MPI_Finalize();
-    cout << "Finalize done " << rc << endl;
     return 0;
 
     //Begin test code
